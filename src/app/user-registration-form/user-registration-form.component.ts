aecf6f9c8b8aeb0832f144690841f0a9a6653783
shiva-bagherifard <shivabagheri1987@gmail.com>
2024-07-10 08:39:30 +0200

    install typeDoc, insert comments in Welcome and user-Registration components

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -3,24 +3,50 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+
+/**
+ * The UserRegistrationFormComponent is used for user registration.
+ */
 @Component({
   selector: 'app-user-registration-form',
   templateUrl: './user-registration-form.component.html',
   styleUrls: ['./user-registration-form.component.scss']
 })
+
+/**
+   * Holds the user's registration data.
+   */
 export class UserRegistrationFormComponent implements OnInit {
 
   @Input() userData = { username: '', password: '', email: '', birthDate: '' };
 
+
+  /**
+   * Creates an instance of UserRegistrationFormComponent.
+   * @param fetchApiData - Service to interact with the API.
+   * @param dialogRef - Reference to the dialog opened.
+   * @param snackBar - Service to show snack bar notifications.
+   * @param router - Router to navigate after registration.
+   */
   constructor(
     public fetchApiData: FetchApiDataService,
     public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
     public snackBar: MatSnackBar
   ) { }
+  
 
+  /**
+   * Initializes the component.
+   */
   ngOnInit(): void {
   }
 
+
+  /**
+   * Registers a new user by sending userData to the backend.
+   */
+
+  
   registerUser(): void {
     this.fetchApiData.userRegistration(this.userData).subscribe(
       (result) => {
--- src/app/welcome-page/welcome-page.component.ts
+++ src/app/welcome-page/welcome-page.component.ts
@@ -3,20 +3,42 @@ import { UserLoginFormComponent } from '../user-login-form/user-login-form.compo
 import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
 
+
+/**
+ * The WelcomePageComponent is the landing page of the application.
+ * It contains methods to open dialogs for user registration and login.
+ */
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
   styleUrls: ['./welcome-page.component.scss']
 })
 export class WelcomePageComponent implements OnInit {
+
+  /**
+   * Creates an instance of WelcomePageComponent.
+   * @param dialog - MatDialog to handle dialog opening.
+   */
   constructor(public dialog: MatDialog) { }
+
+  /**
+   * Initializes the component.
+   */
   ngOnInit(): void {
   }
+
+  /**
+   * Opens the user registration dialog.
+   */
   openUserRegistrationDialog(): void {
     this.dialog.open(UserRegistrationFormComponent, {
       width: '280px'
     });
   }
+
+  /**
+   * Opens the user login dialog.
+   */
 openUserLoginDialog(): void {
     this.dialog.open(UserLoginFormComponent, {
       width: '280px'
